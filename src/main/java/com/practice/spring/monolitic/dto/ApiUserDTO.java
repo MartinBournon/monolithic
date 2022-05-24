857f309a8af75be7f7f6241538fbb65c6534ab48
MartinBournon <martinbournon@gmail.com>
2022-05-24 17:15:01 -0300

    added ApiUser model and DTO with validation

---
+++ src/main/java/com/practice/spring/monolitic/dto/ApiUserDTO.java
@@ -0,0 +1,20 @@
+package com.practice.spring.monolitic.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ApiUserDTO {
+    @NotNull
+    @NotBlank
+    private String username;
+    @NotNull
+    @NotBlank
+    private String password;
+}
